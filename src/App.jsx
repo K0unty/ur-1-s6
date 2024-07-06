3086e10056ffd9ef9fe0a86699ba52be60eb24a3
K0unty <kountiapi@pm.me>
2024-07-06 22:39:20 +0530

    seperationOfConcerns

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
-import { CoreConcept } from "./components/CoreConcept.jsx";
+import { CoreConcept } from "./components/CoreConcepts.jsx";
 import TabButton from "./components/TabButton.jsx";
 
 import { EXAMPLES } from "./data.js";
